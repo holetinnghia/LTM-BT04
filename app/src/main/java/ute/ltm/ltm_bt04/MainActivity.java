5f7621401b821d6328875efbda3bd128c76729e1
holetinnghia <holetinnghia@icloud.com>
2025-11-20 09:36:46 +0700

    SQLite

--- app/src/main/java/ute/ltm/ltm_bt04/MainActivity.java
+++ app/src/main/java/ute/ltm/ltm_bt04/MainActivity.java
@@ -1,24 +1,30 @@
 package ute.ltm.ltm_bt04;
 
-import android.os.Bundle;
+import ute.ltm.ltm_bt04.sqlite.*;
 
-import androidx.activity.EdgeToEdge;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnGoToSQLite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnGoToSQLite = findViewById(R.id.btnGoToSQLite);
+        btnGoToSQLite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SQLiteActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
